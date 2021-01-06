80755854e502eff8911fff53bb4ac1b9bc9b539e
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2021-01-06 14:35:46 +0200

    35 MatTableDataSource MatTable

--- src/app/view/tasks/tasks.component.ts
+++ src/app/view/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {DataHandlerService} from '../../services/data-handler.service';
 import {Task} from '../../model/interfaces';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-tasks',
@@ -9,15 +10,34 @@ import {Task} from '../../model/interfaces';
 })
 export class TasksComponent implements OnInit {
   tasks: Task[] = [];
+  displayedColumns: string[] = ['color', 'id', 'title', 'date', 'priority', 'category'];
+  /*container for table data from tasks[]*/
+  dataSource: MatTableDataSource<Task> = new MatTableDataSource<Task>();
 
   constructor(private data: DataHandlerService) {
   }
 
   ngOnInit(): void {
     this.data.taskSubject.subscribe(tasks => this.tasks = tasks);
+    /*update data source if data of tasks updated*/
+    this.refreshTable();
   }
 
   completedTask(task: Task): void {
     task.completed = !task.completed;
+  }
+
+  private refreshTable(): void {
+    this.dataSource.data = this.tasks;
+  }
+
+  getPriorityColor(task: Task): string {
+    let color = '#fff';
+    if (task.completed) {
+      color = '#F8F9FA';
+    } else if (task.priority && task.priority.color) {
+      color = task.priority.color;
+    }
+    return color;
   }
 }
