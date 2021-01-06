ede81ec9d7d5bb079a8b9d4c5188e871bd9f4714
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2021-01-06 16:50:04 +0200

    35 Paginator!: MatSort!: @ViewChild()

--- src/app/view/tasks/tasks.component.ts
+++ src/app/view/tasks/tasks.component.ts
@@ -1,18 +1,22 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {DataHandlerService} from '../../services/data-handler.service';
 import {Task} from '../../model/interfaces';
 import {MatTableDataSource} from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
-export class TasksComponent implements OnInit {
+export class TasksComponent implements OnInit, AfterViewInit {
   tasks: Task[] = [];
   displayedColumns: string[] = ['color', 'id', 'title', 'date', 'priority', 'category'];
-  /*container for table data from tasks[]*/
+  /*container for table data from tasks[] ps. it can be db or any data source*/
   dataSource: MatTableDataSource<Task> = new MatTableDataSource<Task>();
+  @ViewChild(MatPaginator, {static: false}) private paginator!: MatPaginator;
+  @ViewChild(MatSort, {static: false}) private sort!: MatSort;
 
   constructor(private data: DataHandlerService) {
   }
@@ -23,12 +27,13 @@ export class TasksComponent implements OnInit {
     this.refreshTable();
   }
 
-  completedTask(task: Task): void {
-    task.completed = !task.completed;
+  ngAfterViewInit(): void {
+    /*init after pain page*/
+    this.addTableObjects();
   }
 
-  private refreshTable(): void {
-    this.dataSource.data = this.tasks;
+  completedTask(task: Task): void {
+    task.completed = !task.completed;
   }
 
   getPriorityColor(task: Task): string {
@@ -39,5 +44,36 @@ export class TasksComponent implements OnInit {
       color = task.priority.color;
     }
     return color;
+  }
+
+  private refreshTable(): void {
+    this.dataSource.data = this.tasks;
+    this.addTableObjects();
+    // @ts-ignore
+    this.dataSource.sortingDataAccessor = (task, colName) => {
+      // по каким полям выполнять сортировку для каждого столбца
+      switch (colName) {
+        case 'priority': {
+          return task.priority ? task.priority.id : null;
+        }
+        case 'category': {
+          return task.category ? task.category.title : null;
+        }
+        case 'date': {
+          return task.date ? task.date : null;
+        }
+
+        case 'title': {
+          return task.title;
+        }
+      }
+    };
+  }
+
+  private addTableObjects(): void {
+    /*component for sort data*/
+    this.dataSource.sort = this.sort;
+    /*update component paginator(count pages or notes)*/
+    this.dataSource.paginator = this.paginator;
   }
 }
