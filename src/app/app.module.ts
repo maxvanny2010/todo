2327ffadcce17729739a4d0af531578f2e3954a2
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2021-01-06 13:03:54 +0200

    34 BrowserAnimationsModule,        MatTableModule,        MatPaginatorModule,        MatSortModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,12 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CategoriesComponent } from './view/categories/categories.component';
-import { TasksComponent } from './view/tasks/tasks.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {CategoriesComponent} from './view/categories/categories.component';
+import {TasksComponent} from './view/tasks/tasks.component';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatSortModule} from '@angular/material/sort';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { TasksComponent } from './view/tasks/tasks.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
